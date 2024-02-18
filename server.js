e8b7fd241ea656d77f63eea733e89441e1b5078d
shashank <shashank78680@gmail.com>
2024-02-18 15:06:30 +0530

    added .env file

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-const port = 5000;
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 5001;
 
 app.listen(port, ()=>{
     console.log(`server started on port ${port}`);
